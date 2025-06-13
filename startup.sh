439e6b2100c8f73401d37aa6426fb8a82dbb5bc2
slacasse63 <137903867+slacasse63@users.noreply.github.com>
2025-06-13 19:14:55 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Mise à jour de pip
 pip install --upgrade pip
-
-# Installation des dépendances
 pip install -r requirements.txt
-
-# Lancement de l’application Flask avec gunicorn
 gunicorn -w 1 -b 0.0.0.0:8000 src.serge.mainserge:app
